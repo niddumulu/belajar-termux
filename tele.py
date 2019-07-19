1b1030628b5e190e202126fcfd1b1cadb3659712
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-19 10:58:33 +0700

    Penambahan API

--- tele.py
+++ tele.py
@@ -1,7 +1,7 @@
 from telethon.sync import TelegramClient 
 
-api_id = api 
-api_hash = 'apihash' 
+api_id = 682610 
+api_hash = '030132b51d598e464419ccee7f20212d' 
 
 kirimpesan = input('Mau mengirim pesan apa? ')
 print ('Pesan terkirim yaitu : ' + kirimpesan)
