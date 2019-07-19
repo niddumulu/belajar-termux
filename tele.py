d32bd7b4aa4afaa722ab7dacb9df7ef1df4c90dc
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-19 11:35:25 +0700

    Penambahan penerima

--- tele.py
+++ tele.py
@@ -4,7 +4,7 @@ api_id = 682610
 api_hash = '030132b51d598e464419ccee7f20212d' 
 
 kirimpesan = input('Mau mengirim pesan apa? ')
-print ('Pesan terkirim yaitu : ' + kirimpesan)
-pesan = kirimpesan
+kirimke = input('Masukkan username atau id group : ')
+print ('Pesan terkirim')
 
-with TelegramClient('anon', api_id, api_hash) as client: client.send_message('niddumulu', pesan)
+with TelegramClient('anon', api_id, api_hash) as client: client.send_message(kirimke, kirimpesan)
