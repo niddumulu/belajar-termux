08aa12bc5bad7802d92473df6738c7de72975569
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-19 11:44:40 +0700

    Perubahan kode print terkirim

--- tele.py
+++ tele.py
@@ -5,6 +5,6 @@ api_hash = '030132b51d598e464419ccee7f20212d'
 
 kirimpesan = input('Mau mengirim pesan apa? ')
 kirimke = input('Masukkan username atau id group : ')
-print ('Pesan terkirim')
 
 with TelegramClient('anon', api_id, api_hash) as client: client.send_message(kirimke, kirimpesan)
+print ('Pesan terkirim')
