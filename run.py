a90a93bcdedf37c047ac5f4f336991c0c5a78905
niddumulu <36333121+niddumulu@users.noreply.github.com>
2019-07-21 00:29:20 +0700

    Update penambahan waktu

--- run.py
+++ run.py
@@ -3,23 +3,28 @@ import time
 from config import *
 
 seconds= float(0)
-minutes= int(0)
+minutes= int(-1)
 hours= int(0)
 
 run = input("Enter r : ")
 while run.lower()=="r":
-  if seconds > 540:
+  if seconds > 529:
       seconds = 0
-      minutes = minutes+1
-      with TelegramClient('anon', api_id, api_hash) as client: client.send_message('dogetipbotgroup', "be active here guys")
-      #print ('Pesan terkirim')     
-  if minutes > 60:
-     minutes = 0
+      minutes = minutes+1      
+      baris = minutes
+      my_file = open("data.txt", "r")
+      puisi = my_file.readlines()
+      dataok = (puisi[baris])
+      print (dataok)
+      my_file.close()
+      with TelegramClient('anon', api_id, api_hash) as client: client.send_message('niddumulu',dataok)
+      #print ('Pesan terkirim')
+  if minutes > 8 :
+     minutes = -1
      hours = hours+1
-       
+     
   os.system('clear')
-  #from coba import *
   seconds = (seconds+1)
   #print (hours," : ",minutes," : ", seconds)
-  print (seconds)
+  print ("menunggu 530 detik ",seconds)
   time.sleep(1)
